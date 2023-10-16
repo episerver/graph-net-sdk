69c33f4d890869e6c672c2f8b330fcc4b47ab6b8
Ted Lundquist <ted.lundquist@optimizely.com>
2023-10-16 09:53:30 +0200

    Refactor out testdata creation

--- APIs/src/Tetsing/EPiServer.ContentGraph.IntegrationTests/QueryTests/SimpleQueryTests.cs
+++ APIs/src/Tetsing/EPiServer.ContentGraph.IntegrationTests/QueryTests/SimpleQueryTests.cs
@@ -11,70 +11,15 @@ using System.Text;
 
 namespace EPiServer.ContentGraph.IntegrationTests.QueryTests
 {
-
-    public class IndexActionData
-    {
-        [JsonPropertyName("ContentType")]
-        public string[]? ContentType { get; set; }
-
-        [JsonPropertyName("Id")]
-        public string? Id { get; set; }
-        
-        [JsonPropertyName("Name___searchable")]
-        public string? NameSearchable { get; set; }
-        
-        [JsonPropertyName("Author")]
-        public string? Author { get; set; }
-        
-        [JsonPropertyName("Status")]
-        public string? Status { get; set; }
-        
-        [JsonPropertyName("RolesWithReadAccess")]
-        public string? RolesWithReadAccess { get; set; }
-
-    }
-    public class IndexAction
-    {
-        [JsonPropertyName("index")]
-        public IndexActionValues? Values { get; set; }
-    }
-    public class IndexActionValues
-    {
-        [JsonPropertyName("_id")]
-        public string? Id { get; set; }
-
-        [JsonPropertyName("language_routing")]
-        public string? LanguageRouting { get; set; }
-    }
-   
-        
     [TestClass]
     public class Index_3_Items_Then_Search : IntegrationFixture
     {
-        public static string generateIndexActionJson(string indexId, string languageRouting, string[] contentTypes, string contentId, string nameSearchable, string author, string status, string roles)
-        {
-            var indexAction = new IndexAction { Values = new IndexActionValues { Id = indexId, LanguageRouting = languageRouting } };
-            var indexActionData = new IndexActionData { ContentType = contentTypes, Id = contentId, NameSearchable = nameSearchable, Author = author, Status = status, RolesWithReadAccess = roles };
-
-            using (var memoryStream = new MemoryStream())
-            using (var writer = new StreamWriter(memoryStream))
-            {
-                
-                writer.Write(JsonSerializer.Serialize(indexAction));
-                writer.Write("\n");
-                writer.Write(JsonSerializer.Serialize(indexActionData));
-                writer.Write("\n");
-                writer.Flush();
-                return Encoding.Default.GetString((memoryStream.ToArray()));
-            }
-
-        }
         [ClassInitialize]
         public static void ClassInitialize(TestContext testContext)
         {
-            var item1 = generateIndexActionJson("1","en", new[] { "Content" }, "content1", "Steve Job", "manv", "Published", "Everyone" );
-            var item2 = generateIndexActionJson("2", "en", new[] { "Content" }, "content2", "Tim Cook", "manv", "Published", "Everyone");
-            var item3 = generateIndexActionJson("3", "en", new[] { "Content" }, "content3", "Alan Turing", "manv", "Published", "Everyone");
+            var item1 = TestDataCreator.generateIndexActionJson("1","en", new[] { "Content" }, "content1", "Steve Job", "manv", "Published", "Everyone" );
+            var item2 = TestDataCreator.generateIndexActionJson("2", "en", new[] { "Content" }, "content2", "Tim Cook", "manv", "Published", "Everyone");
+            var item3 = TestDataCreator.generateIndexActionJson("3", "en", new[] { "Content" }, "content3", "Alan Turing", "manv", "Published", "Everyone");
             SetupData(item1 + item2 + item3);
         }
         [TestMethod]
---
+++ APIs/src/Tetsing/EPiServer.ContentGraph.IntegrationTests/TestSupport/TestDataCreator.cs
@@ -0,0 +1,61 @@
+﻿using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace EPiServer.ContentGraph.IntegrationTests.TestSupport
+{
+    public class TestDataCreator
+    {
+        public static string generateIndexActionJson(string indexId, string languageRouting, string[] contentTypes, string contentId, string nameSearchable, string author, string status, string roles)
+        {
+            var indexAction = new IndexAction { Values = new IndexActionValues { Id = indexId, LanguageRouting = languageRouting } };
+            var indexActionData = new IndexActionData { ContentType = contentTypes, Id = contentId, NameSearchable = nameSearchable, Author = author, Status = status, RolesWithReadAccess = roles };
+
+            using (var memoryStream = new MemoryStream())
+            using (var writer = new StreamWriter(memoryStream))
+            {
+                writer.Write(JsonSerializer.Serialize(indexAction));
+                writer.Write("\n");
+                writer.Write(JsonSerializer.Serialize(indexActionData));
+                writer.Write("\n");
+                writer.Flush();
+                return Encoding.Default.GetString((memoryStream.ToArray()));
+            }
+        }
+    }
+
+    public class IndexActionData
+    {
+        [JsonPropertyName("ContentType")]
+        public string[]? ContentType { get; set; }
+
+        [JsonPropertyName("Id")]
+        public string? Id { get; set; }
+
+        [JsonPropertyName("Name___searchable")]
+        public string? NameSearchable { get; set; }
+
+        [JsonPropertyName("Author")]
+        public string? Author { get; set; }
+
+        [JsonPropertyName("Status")]
+        public string? Status { get; set; }
+
+        [JsonPropertyName("RolesWithReadAccess")]
+        public string? RolesWithReadAccess { get; set; }
+
+    }
+    public class IndexAction
+    {
+        [JsonPropertyName("index")]
+        public IndexActionValues? Values { get; set; }
+    }
+    public class IndexActionValues
+    {
+        [JsonPropertyName("_id")]
+        public string? Id { get; set; }
+
+        [JsonPropertyName("language_routing")]
+        public string? LanguageRouting { get; set; }
+    }
+}
