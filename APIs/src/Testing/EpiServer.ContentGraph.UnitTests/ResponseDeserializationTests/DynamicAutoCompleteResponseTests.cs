46ab118b71e0e7049f60f67b3aff8f3ac8a89fde
manh nguyen <manh.nguyen@episerver.com>
2024-03-18 14:44:31 +0700

    Deseralization errors
    - Add simple unit tests for facets & autocompletes response

---
+++ APIs/src/Testing/EpiServer.ContentGraph.UnitTests/ResponseDeserializationTests/DynamicAutoCompleteResponseTests.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+using Newtonsoft.Json;
+using EPiServer.ContentGraph.Api.Result;
+
+namespace EpiServer.ContentGraph.UnitTests.DeserializationTets
+{
+    public class DynamicAutoCompleteResponseTests
+    {
+        const string response = "{\"data\":{\"Object\":{\"autocomplete\":{\"Categories\":{\"ProviderName\":[\"Sample Category\"]},\"WebsiteUrl\":[\"http://example.com/\",\"https://test.com/\"]},\"total\":2}},\"extensions\":{\"correlationId\":\"866380417b4f1fb0\",\"cost\":72,\"costSummary\":[\"CompanyBlock(72)=basicFilter(1)*2+autocomplete(2)*35\"]}}";
+        [Fact]
+        public void nested_aotucompletes_should_convert_successfully()
+        {
+            var results = JsonSerializer.CreateDefault().Deserialize<ContentGraphResult<object>>(new JsonTextReader(new StringReader(response)));
+            var autoCompletesDict = results.Content.AutoComplete;
+            Assert.True(autoCompletesDict.Count.Equals(2));
+
+            Assert.NotNull(autoCompletesDict["Categories.ProviderName"]);
+            Assert.True(autoCompletesDict["Categories.ProviderName"].Count().Equals(1));
+
+            Assert.NotNull(autoCompletesDict["WebsiteUrl"]);
+            Assert.True(autoCompletesDict["WebsiteUrl"].Count().Equals(2));
+        }
+    }
+}
---
+++ APIs/src/Testing/EpiServer.ContentGraph.UnitTests/ResponseDeserializationTests/DynamicFacetsResponseTests.cs
@@ -0,0 +1,27 @@
+﻿using Xunit;
+using Newtonsoft.Json;
+using EPiServer.ContentGraph.Api.Result;
+
+namespace EpiServer.ContentGraph.UnitTests.DeserializationTets
+{
+    public class DynamicFacetsResponseTests
+    {
+        const string response = "{\"data\":{\"Object\":{\"items\":[{\"Name\":\"Coreware\"}],\"facets\":{\"Categories\":{\"Id\":[{\"name\":\"100\",\"count\":3}],\"Language\":{\"Name\":[{\"name\":\"en\",\"count\":3}]}},\"Name\":[{\"name\":\"Farfetch\",\"count\":3},{\"name\":\"Geta\",\"count\":3}]},\"total\":1154}},\"extensions\":{\"correlationId\":\"8662284c0889108a\",\"cost\":62,\"costSummary\":[\"CompanyBlock(62)=basicFilter(1)*2+facets(2)*30\"]}}";
+        [Fact]
+        public void nested_facets_response_should_convert_successfully()
+        {
+            var results = JsonSerializer.CreateDefault().Deserialize<ContentGraphResult<object>>(new JsonTextReader(new StringReader(response)));
+            var facetsDict = results.Content.Facets;
+            Assert.True(facetsDict.Count.Equals(3));
+
+            Assert.NotNull(facetsDict["Categories.Id"]);
+            Assert.True(facetsDict["Categories.Id"].Count().Equals(1));
+
+            Assert.NotNull(facetsDict["Categories.Language.Name"]);
+            Assert.True(facetsDict["Categories.Language.Name"].Count().Equals(1));
+
+            Assert.NotNull(facetsDict["Name"]);
+            Assert.True(facetsDict["Name"].Count().Equals(2));
+        }
+    }
+}
